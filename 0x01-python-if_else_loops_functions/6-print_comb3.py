8d391d18840b35aeac1fe8ff782825900f82f759
Gabriel Muthenya <gmthex@gmail.com>
2023-05-09 09:06:29 -0700

    Chec if is upper of lower

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+for first in range(10):
+    for last in range(first + 1, 10):
+        if first == 8 and last == 9:
+            print("{:d}{:d}".format(first, last))
+        print("{:d}{:d}".format(first, last), end=", ")
