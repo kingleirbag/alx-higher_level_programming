3861936217bb6ca7fc072fd6c25e9e5eef03c378
Gabriel Muthenya <gmthex@gmail.com>
2023-05-09 09:33:26 -0700

    Print last digit

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,6 @@
 for first in range(10):
     for last in range(first + 1, 10):
         if first == 8 and last == 9:
-            print("{:d}{:d}".format(first, lasti))
+            print("{:d}{:d}".format(first, last))
         else:
             print("{:d}{:d}".format(first, last), end=", ")
