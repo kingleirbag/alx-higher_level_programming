7ae2cb87716f56f753936ef66074ebf3dd07b558
Gabriel Muthenya <gmthex@gmail.com>
2023-05-11 09:49:35 -0700

    Hiddedn dir

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -3,7 +3,7 @@
 if __name__ == "__main__":
     import hidden_4
 
-    hidden_dirs = = dir(hidden_4)
+    hidden_dirs = dir(hidden_4)
     for hidden_dir in hidden_dirs:
         if hidden_dir[:2] != "__":
             print(hidden_dir)
