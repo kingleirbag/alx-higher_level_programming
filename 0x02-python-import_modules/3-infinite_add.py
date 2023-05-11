1468617df8d80e9e995d8514cda82208ae2e6a06
Gabriel Muthenya <gmthex@gmail.com>
2023-05-11 07:49:15 -0700

    Sum total

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -4,6 +4,9 @@ if __name__ == "__main__":
     import sys
 
      arguments = sys.argv[1:]
-     sum_total = sum(int(argument) for argument in arguments)
+     sum_total = 0
+
+     for argument in arguments):
+         sum_total += int(argument)
 
      print(sum_total)
