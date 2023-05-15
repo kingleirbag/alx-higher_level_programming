c96ecad57328dda81356471260dd5aff7ddf8d90
Gabriel Muthenya <gmthex@gmail.com>
2023-05-15 11:16:49 -0700

    Fix

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -7,18 +7,4 @@
  */
 void print_python_list_info(PyObject *p)
 {
-	int pysize, memaloc, count;
-	PyObject *obj;
-
-	pysize = Py_SIZE(p);
-	memaloc = ((PyListObject *)p)->allocated;
-
-	printf("[*] Size of the Python List = %d\n", pysize);
-	printf("[*] Allocated = %d\n", memaloc);
-	for (count = 0; count < size; count++)
-	{
-		printf("Element %d: ", count);
-		obj = PyList_GetItem(p, count);
-		printf("%s\n", Py_TYPE(obj)->tp_name);
-	}
 }
