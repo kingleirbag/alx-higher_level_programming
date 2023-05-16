456b911ee0d7e2826b0f54803df52bed5586d677
Gabriel Muthenya <gmthex@gmail.com>
2023-05-16 00:54:40 -0700

    Python

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -7,4 +7,19 @@
  */
 void print_python_list_info(PyObject *p)
 {
+	int list_size, mem_alloc, count;
+	PyObject *py_obj;
+
+	list_size = Py_SIZE(p);
+	mem_alloc = ((PyListObject *)p)->allocated;
+
+	printf("[*] Size of the Python List = %d\n", list_size);
+	printf("[*] Allocated = %d\n", mem_alloc);
+
+	for (count = 0; count < list_size; count++)
+	{
+		printf("Element %d: ", count);
+		py_obj = PyList_GetItem(p, count);
+		printf("%s\n", Py_TYPE(py_obj)->tp_name);
+	}
 }
