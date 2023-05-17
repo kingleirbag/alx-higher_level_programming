df0d3dd869e4a8b9b27a57374c1a0b63aecf4b8c
Gabriel Muthenya <gmthex@gmail.com>
2023-05-17 06:02:39 -0700

    Roman to int

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,7 +3,7 @@ def best_score(a_dictionary):
     if not isinstance(a_dictionary, dict) or len(a_dictionary) == 0:
         return None
     max_key = list(a_dictionary.keys())[0]
-    maximum_value = = a_dictionary[max_key]
+    maximum_value = a_dictionary[max_key]
     for i, j in a_dictionary.items():
         if j > maximum_value:
             maximum_value = j
