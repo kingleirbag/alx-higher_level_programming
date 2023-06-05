db596f1d9f03f3115721835662e922ac24fd92d0
Gabriel Muthenya <gmthex@gmail.com>
2023-06-05 10:35:22 -0700

    Update the rectangle height and width

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""
+Defines a class Rectangle
+"""
+
+class Rectangle:
+    """ Class  rectangle represent"""
+    def __init__(self, width=0, height=0):
+        """Initialize the rectangle"""
+        self.height = height
+        self.width = width
+
+    @property
+    def width(self):
+        """get the private instance attribute width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """set the private instance attribute width"""
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """get the private instance attribute height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """set the private instance attribute height"""
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
