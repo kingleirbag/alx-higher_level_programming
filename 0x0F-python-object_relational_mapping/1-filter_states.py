954dc97182d8eb793abfa9c0091c797cb58c4bbd
Gabriel Muthenya <gabriel@bildit.co>
2023-08-20 20:47:22 +0300

    fix update

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""lists all states from the database"""
+"""a script that lists all states with a name starting with
+N (upper N) from the database hbtn_0e_0_usa:"""
 import MySQLdb
 import sys
 
--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-"""lists all states from the database"""
+"""
+a script that takes in an argument and displays all values in the states
+table of hbtn_0e_0_usa where name matches the argument.
+"""
 import MySQLdb
 import sys
 
