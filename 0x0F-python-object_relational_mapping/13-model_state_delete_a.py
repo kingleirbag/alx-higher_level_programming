6fafba722fc3d979252142d58987bd3aab645fd5
Gabriel Muthenya <gmthex@gmail.com>
2023-08-20 10:14:35 -0700

    Delete states

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-"""a script that changes the name of a State
-object from the database hbtn_0e_6_usa
+""" a script that deletes all State objects with a
+name containing the letter a from the database hbtn_0e_6_usa
 """
 import sys
 from model_state import Base, State
@@ -14,6 +14,6 @@ if __name__ == "__main__":
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    new_instance = session.query(State).filter_by(id=2).first()
-    new_instance.name = 'New Mexico'
+    for instance in session.query(State).filter(State.name.like('%a%')):
+        session.delete(instance)
     session.commit()
