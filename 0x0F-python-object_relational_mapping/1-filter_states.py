9a5e74b7aa1847ad5c0b13f5f57bafde9d99af23
Gabriel Muthenya <gmthex@gmail.com>
2023-08-20 09:38:51 -0700

    Filter states by user input

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -10,8 +10,8 @@ if __name__ == "__main__":
     cursor = db.cursor()
     cursor.execute("""SELECT * FROM states WHERE name
                 LIKE BINARY 'N%' ORDER BY states.id""")
-    rows = cursor.fetchall()
-    for row in rows:
-        print(row)
+    allstates = cursor.fetchall()
+    for state in allstates:
+        print(state)
     cursor.close()
     db.close()
--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""lists all states from the database """
+"""lists all states from the database"""
 import MySQLdb
 import sys
 
@@ -8,7 +8,8 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3], port=3306)
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM states")
+    cursor.execute("SELECT * FROM states WHERE name LIKE BINARY '{}'"
+                .format(sys.argv[4]))
     allstates = cursor.fetchall()
     for state in allstates:
         print(state)
