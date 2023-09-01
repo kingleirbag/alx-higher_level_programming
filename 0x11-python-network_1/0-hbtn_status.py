e0e6da3648de81c92ceba3c5d0d266cddc8c1d69
Gabriel Muthenya <gmthex@gmail.com>
2023-09-01 12:18:27 -0700

    Fix

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
-# A PIython script that fetches https://intranet.hbtn.io/status
-import urllib.request
+"""A script that
+- fetches https://alx-intranet.hbtn.io/status.
+"""
 
-if __name__ == "__main__":
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as res:
+
+if __name__ == '__main__':
+    import urllib.request
+
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as res:
         content = res.read()
         print("Body response:")
         print("\t- type: {}".format(type(content)))
